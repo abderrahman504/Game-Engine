3241592dbc380c98f09cd8746df820dfff260549
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-02 23:40:00 +0300

    Iss #3 Finished Node3D class

---
+++ src/Engine/Nodes/Node3D/Node3D.cpp
@@ -0,0 +1,53 @@
+#include "Node3D.h"
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+using namespace Engine::Nodes;
+using namespace Engine::DataTypes;
+
+Node3D::Node3D() : Node(){
+    position = Vector3::ZERO;
+    orientation = Quaternion::FORWARD;
+    scale = Vector3::ONE;
+    setName("Node3D");
+}
+
+
+Vector3 Node3D::Position(){return position;}
+void Node3D::Position(Vector3 position){this->position = position;}
+Quaternion Node3D::Orientation(){return orientation;}
+void Node3D::Orientation(Quaternion quaterion){this->orientation = quaterion;}
+Vector3 Node3D::Scale(){return scale;}
+void Node3D::Scale(Vector3 scale){this->scale = scale;}
+
+
+void Node3D::lookAt(Vector3 point, Vector3 up){
+    lookTowards(point - position, up);
+}
+
+void Node3D::lookTowards(Vector3 direction, Vector3 up){
+    Vector3 vec = Vector3::FORWARD;
+    //Rotate so your face points towards direction
+    rotateAround(vec.cross(direction), vec.angle_to(direction));
+    //Rotate so your top points towards up
+    Vector3 properUp = up - direction * up.dot(direction); 
+    vec = Vector3::UP;
+    rotateAround(vec.cross(properUp), vec.angle_to(properUp));
+}
+
+void Node3D::rotateAround(Vector3 axis, float angleRad){
+    Quaternion newRotation = Quaternion(angleRad, axis);
+    orientation = orientation * newRotation;
+}
+
+void Node3D::propegateDraw(){
+    glPushMatrix();
+    glTranslatef(position.x, position.y, position.z);
+    float angle;
+    Vector3 axis;
+    orientation.angle_and_axis(&angle, &axis);
+    glRotatef(angle, axis.x, axis.y, axis.z);
+    glScalef(scale.x, scale.y, scale.z);
+    Node::propegateDraw();
+    glPopMatrix();
+}
---
+++ src/Engine/Nodes/Node3D/Node3D.h
@@ -0,0 +1,38 @@
+#ifndef NODE3D_GUARD
+#define NODE3D_GUARD
+#include "../Node.h"
+#include "../../DataTypes.h"
+
+using namespace Engine::DataTypes;
+
+namespace Engine{
+namespace Nodes{
+class Node3D : public Node
+{
+    protected:
+    Vector3 position;
+    Quaternion orientation;
+    Vector3 scale;
+    
+    public:
+    Node3D();
+    Vector3 Position();
+    void Position(Vector3 position);
+    Quaternion Orientation();
+    void Orientation(Quaternion quaterion);
+    Vector3 Scale();
+    void Scale(Vector3 scale);
+    
+    //Orients the node3d to look at position ```point``` with its up direction facing ```up```.
+    void lookAt(Vector3 point, Vector3 up);
+    //Orients the node3d to look towards ```direction``` with its up direction facing ```up```.
+    void lookTowards(Vector3 direction, Vector3 up);
+    //Rotates the node3d around ```axis``` by ```angle``` radians.
+    void rotateAround(Vector3 axis, float angleRad);
+
+    void propegateDraw();
+};
+
+}}
+
+#endif
