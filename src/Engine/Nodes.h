2f6f68f76934b2735d688e42e7da08c470f934bf
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-02 22:34:11 +0300

    Header that includes all Node3D classes

--- src/Engine/Nodes.h
+++ src/Engine/Nodes.h
@@ -1,12 +1,7 @@
 #ifndef NODES_GUARD
 #define NODES_GUARD
 #include "Nodes/Node.h"
-// #include "Nodes/Node3D.h"
-// #include "Nodes/Mesh3D.h"
-// #include "Nodes/Camera3D.h"
-// #include "Nodes/BoxCollider3D.h"
-// #include "Nodes/PhysicsBody3D.h"
-// #include "Nodes/KinematicBody3D.h"
+#include "Nodes/IncludeNode3D.h"
 
 
 #endif
---
+++ src/Engine/Nodes/IncludeNode3D.h
@@ -0,0 +1,9 @@
+#ifndef NODE3D_INCLUDES_GUARD
+#define NODE3D_INCLUDES_GUARD
+#include "Node3D/Node3D.h"
+// #include "Node3D/Mesh3D.h"
+// #include "Node3D/Camera3D.h"
+// #include "Node3D/PhysicsBody3D.h"
+// #include "Node3D/KinematicBody3D.h"
+// #include "Node3D/BoxCollider3D.h"
+#endif
