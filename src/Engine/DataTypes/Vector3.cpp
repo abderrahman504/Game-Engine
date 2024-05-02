ed8f92a66c045948cf566ade57ab3063924be145
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-02 14:07:43 +0300

    #2 Initial Vector3 type

---
+++ src/Engine/DataTypes/Vector3.cpp
@@ -0,0 +1,48 @@
+#include "Vector3.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+namespace Engine{
+namespace DataTypes{
+    const Vector3 Vector3::ZERO(0, 0, 0);
+    const Vector3 Vector3::ONE(1, 1, 1);
+    const Vector3 Vector3::LEFT(-1, 0, 0);
+    const Vector3 Vector3::RIGHT(1, 0, 0);
+    const Vector3 Vector3::UP(0, 1, 0);
+    const Vector3 Vector3::DOWN(0, -1, 0);
+    const Vector3 Vector3::FORWARD(0, 0, 1);
+    const Vector3 Vector3::BACK(0, 0, -1);
+    
+    
+    Vector3::Vector3(float x, float y, float z): x(x), y(y), z(z){}
+
+    Vector3 Vector3::operator+(Vector3 other){
+        return Vector3(x+other.x, y+other.y, z+other.z);
+    }
+    Vector3 Vector3::operator-(Vector3 other){
+        return Vector3(x-other.x, y-other.y, z-other.z);
+    }
+    Vector3 Vector3::operator*(float scalar){
+        return Vector3(x*scalar, y*scalar, z*scalar);
+    }
+    Vector3 Vector3::operator/(float scalar){
+        return Vector3(x/scalar, y/scalar, z/scalar);
+    }
+    float Vector3::dot(Vector3 other){
+        return x*other.x + y*other.y + z*other.z;
+    }
+    Vector3 Vector3::cross(Vector3 other){
+        return Vector3(y*other.z - z*other.y, z*other.x - x*other.z, x*other.y - y*other.x);
+    }
+    //Returns the angle between this and other
+    float Vector3::angle_to(Vector3 other){
+        return acos(dot(other)/(length()*other.length()));
+    }
+    float Vector3::length(){
+        return sqrt(x*x + y*y + z*z);
+    }
+    Vector3 Vector3::normalize(){
+        return (*this)/length();
+    }
+
+}}
--- src/Engine/DataTypes/Vector3.h
+++ src/Engine/DataTypes/Vector3.h
@@ -1,5 +1,24 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
+namespace Engine{
+namespace DataTypes{
+    struct Vector3
+    {
+        static const Vector3 ZERO,ONE, LEFT, RIGHT, UP, DOWN, FORWARD, BACK; 
+        float x,y,z;
+        Vector3(float x, float y, float z);
+        Vector3 operator+(Vector3 other);
+        Vector3 operator-(Vector3 other);
+        Vector3 operator*(float scalar);
+        Vector3 operator/(float scalar);
+        float dot(Vector3 other);
+        Vector3 cross(Vector3 other);
+        //Returns the angle between this and other
+        float angle_to(Vector3 other);
+        float length();
+        Vector3 normalize();
+    };
+}}
 
 #endif
