bb544862cf3871bec13a1976c057c3c4da344c37
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-09 18:28:12 +0300

    Give the sun mesh an orange material

--- src/Game/MainGameScene.cpp
+++ src/Game/MainGameScene.cpp
@@ -8,6 +8,13 @@ Engine::Nodes::Node* MainGameScene::constructTree()
     Node* root = new Node();
     root->setName("Solar System");
     SphereMesh* sun = new SphereMesh(50, 100);
+    Material* sunMaterial = sun->material;
+    sunMaterial->color[0] = 1;
+    sunMaterial->color[1] = 0.8;
+    sunMaterial->color[2] = 0.2;
+    sunMaterial->ambient_diffuse = 1;
+    sunMaterial->shininess = 0;
+    sunMaterial->specular = 0;
     sun->setName("Sun");
     root->addChild(sun);
     sun->Position(Vector3(0, 0, -100));
