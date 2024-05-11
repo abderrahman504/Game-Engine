2f8995871e6675c1905315559d85529873268554
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-11 10:36:09 +0300

    Iss #5 Add comments to help whoever will implement the planet logic.

--- src/Game/MainGameScene.cpp
+++ src/Game/MainGameScene.cpp
@@ -15,9 +15,11 @@ Engine::Nodes::Node* MainGameScene::constructTree()
     sunMaterial->ambient_diffuse = 1;
     sunMaterial->shininess = 0;
     sunMaterial->specular = 0;
+    sun->Position(Vector3(0, 0, -100));
     sun->setName("Sun");
     root->addChild(sun);
-    sun->Position(Vector3(0, 0, -100));
-
+    //Create 8 planet objects and make them children of the sun
+    //For each planet you need to set its material, orbit radius and orbit speed. Don't worry about setting position.
+    //You can create another planet object for the moon and make it a child of earth.
     return root;
 }
--- src/Game/Planet.cpp
+++ src/Game/Planet.cpp
@@ -8,4 +8,5 @@ void Planet::idle(double deltaTime)
     //Update planet angle every frame
     //multiply orbit speed by deltaTime to get the change in orbit angle this frame
     //Increase the orbit angle and calculate the position based on the angle and radius
+    //Assume the planet orbits the origin point, which is where the sun is.
 }
