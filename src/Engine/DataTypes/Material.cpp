b9fac31722331aa974083c921fd03e753683aee2
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-22 13:43:27 +0300

    Material uses Color datatype instead of float array

--- src/Engine/DataTypes/Material.cpp
+++ src/Engine/DataTypes/Material.cpp
@@ -5,12 +5,10 @@ using namespace Engine::DataTypes;
 
 Material::Material(){}
 
-Material::Material(float r, float g, float b, float a, float amb, float spec, float shine){
-    color[0] = r;
-    color[1] = g;
-    color[2] = b;
-    color[3] = a;
-    ambient_diffuse = amb;
+Material::Material(Color color, float amb_dif, float emi, float spec, int shine){
+    this->color = color;
+    ambient_diffuse = amb_dif;
+    emission = emi;
     specular = spec;
     shininess = shine;
 }
