c651402edd1bb4d0d13d37ca26b4423280e9aa51
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-22 13:42:52 +0300

    Color Datatype

---
+++ src/Engine/DataTypes/Color.h
@@ -0,0 +1,30 @@
+#ifndef COLOR_GUARD
+#define COLOR_GUARD
+
+namespace Engine{
+namespace DataTypes{
+
+    struct Color
+    {
+        static const Color WHITE, BLACK, RED, GREEN, BLUE;
+        float r,g,b,a;
+
+        Color();
+        Color(float r, float g, float b, float a);
+        Color(int r, int g, int b, int a);
+        Color(int rgb_hexa);
+
+        void setRGB(float r, float g, float b, float a);
+        void setRGB(int r, int g, int b, int a);
+        void setRGB(int hexa);
+
+        // void setHSV(float h, float s, float v, float a);
+        // void setHSV(int h, int s, int v, int a);
+        // void setHSV(int hexa);
+
+
+    };
+
+}}
+
+#endif
