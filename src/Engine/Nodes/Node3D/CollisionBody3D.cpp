768084dee8d21b4b8131f5d331b519179900976b
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-25 17:18:15 +0300

    Iss #10 CollisionBody3D class

---
+++ src/Engine/Nodes/Node3D/CollisionBody3D.cpp
@@ -0,0 +1,13 @@
+#include "CollisionBody3D.h"
+
+namespace Engine
+{
+namespace Nodes
+{
+    CollisionBody3D::CollisionBody3D() : Node3D()
+    {
+        setName("CollisionBody3D");
+    }
+
+    void CollisionBody3D::onCollision(CollisionBody3D* other){}
+}}
---
+++ src/Engine/Nodes/Node3D/CollisionBody3D.h
@@ -0,0 +1,22 @@
+#ifndef COLLISION_BODY_3D_GUARD
+#define COLLISION_BODY_3D_GUARD
+#include "Node3D.h"
+
+namespace Engine
+{
+namespace Nodes
+{
+    class CollisionBody3D : public Node3D
+    {
+        public:
+        unsigned short collisionLayer = 0;
+        unsigned short collisionMask = 0;
+        bool disabled = false;
+
+        CollisionBody3D();
+        virtual void onCollision(CollisionBody3D* other);
+    };
+}}
+
+
+#endif
