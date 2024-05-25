2df718337578c727493ba32a4b203cdc221db956
abderrahman504 <rahmankhsherb@gmail.com>
2024-05-25 17:19:28 +0300

    Iss #10 Collider3D class

---
+++ src/Engine/Nodes/Node3D/Collider3D.cpp
@@ -0,0 +1,11 @@
+#include "Collider3D.h"
+
+namespace Engine
+{
+namespace Nodes
+{
+    Collider3D::Collider3D() : Node3D()
+    {
+        setName("Collider3D");
+    }
+}}
---
+++ src/Engine/Nodes/Node3D/Collider3D.h
@@ -0,0 +1,17 @@
+#ifndef COLLIDER_3D_GUARD
+#define COLLIDER_3D_GUARD
+#include "Node3D.h"
+
+namespace Engine
+{
+namespace Nodes
+{
+    class Collider3D : public Node3D
+    {
+        public:
+        Collider3D();
+    };
+}}
+
+
+#endif
