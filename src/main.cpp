e40be4a3d8e17f5116edd5662d3ffb512d698eaa
abderrahman504 <rahmankhsherb@gmail.com>
2024-06-09 16:58:00 +0300

    main runs light testing scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Engine.h"
 #include "Game/MainGameScene.h"
+#include "Game/LightTestScene.cpp"
 #include <stdio.h>
 
 using namespace Engine;
@@ -8,7 +9,8 @@ using namespace Engine::DataTypes;
 int main(int argc, char** argv)
 {
     initEngine(argc, argv);
-    SceneHead* scene = new Game::MainGameScene();
+    // SceneHead* scene = new Game::MainGameScene();
+    SceneHead* scene = new LightTestScene();
     setMainScene(scene);
     return 0;
 }
