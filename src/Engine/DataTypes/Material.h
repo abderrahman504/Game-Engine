7d102da06ccf7d580afba6ff62773153d1c84524
abderrahman504 <rahmankhsherb@gmail.com>
2024-06-10 23:28:25 +0300

    Increase default material shininess

--- src/Engine/DataTypes/Material.h
+++ src/Engine/DataTypes/Material.h
@@ -9,7 +9,7 @@ namespace DataTypes{
         //All these values must be in range [0,1]
         float ambient_diffuse = 1; //Represents the percentage of ambient & diffuse light reflected by this material.
         float specular = 1; // Reperesents the percentage of specular light reflected by this material.
-        float shininess = 1; //Represents the shininess of the material
+        float shininess = 80; //Represents the shininess of the material
         
         Material();
         Material(float r, float g, float b, float a, float amb, float spec, float shine);
