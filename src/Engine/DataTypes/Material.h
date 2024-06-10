2c9840ae0da1280d992b1a6e401cb6f2f278a87c
abderrahman504 <rahmankhsherb@gmail.com>
2024-06-10 23:27:33 +0300

    Materials have a white color by default

--- src/Engine/DataTypes/Material.h
+++ src/Engine/DataTypes/Material.h
@@ -5,7 +5,7 @@ namespace Engine{
 namespace DataTypes{
 
     struct Material{
-        float color[4] = {0,0,0,1}; //Sets the color of the material. The fourth value is the alpha value.
+        float color[4] = {1,1,1,1}; //Sets the color of the material. The fourth value is the alpha value.
         //All these values must be in range [0,1]
         float ambient_diffuse = 1; //Represents the percentage of ambient & diffuse light reflected by this material.
         float specular = 1; // Reperesents the percentage of specular light reflected by this material.
