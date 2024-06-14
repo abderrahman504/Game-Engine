950cec107209e617f208110a96c0ef911bff6c4a
abderrahman504 <rahmankhsherb@gmail.com>
2024-06-14 22:43:37 +0300

    Include DiskMesh3D

--- src/Engine/Nodes/Node3D/DiskMesh3D.cpp
+++ src/Engine/Nodes/Node3D/DiskMesh3D.cpp
@@ -25,22 +25,23 @@ void buildVertices(float r1, float r2, float thickness, int resolution, float **
     *countIndeces = new int[4];
     *indeces = new unsigned int*[4];
     //Top and Bottom side indeces
-    (*indeces)[0] = new unsigned int(2* (resolution+1));
+    (*indeces)[0] = new unsigned int[2* (resolution+1)];
     (*countIndeces)[0] = 2* (resolution+1);
-    (*indeces)[1] = new unsigned int(2* (resolution+1));
+    (*indeces)[1] = new unsigned int[2* (resolution+1)];
     (*countIndeces)[1] = 2* (resolution+1);
 
     //Wall indeces
-    (*indeces)[2] = new unsigned int(2* (resolution+1));
+    (*indeces)[2] = new unsigned int[2* (resolution+1)];
     (*countIndeces)[2] = 2* (resolution+1);
-    (*indeces)[3] = new unsigned int(2* (resolution+1));
+    (*indeces)[3] = new unsigned int[2* (resolution+1)];
     (*countIndeces)[3] = 2* (resolution+1);
 
     //Verteces allocation: each side has 2*(resolution+1) verteces. There are 2 sides
     *vertices = new float[4 * (resolution+1) * 3];
     
     //Setting verteces
-    
+    //[0 : 2*resolution-1] are bottom side verteces
+    //[2*resolution : 4*resolution-1] are top side verteces]
     for(int i=0; i < resolution; i++)
     {
         float angle = 2 * PI * i / resolution;
@@ -55,11 +56,11 @@ void buildVertices(float r1, float r2, float thickness, int resolution, float **
 
         //top side
         (*vertices)[2*(resolution+i)*3 + 0] = r1 * cos(angle);
-        (*vertices)[2*(resolution+i)*3 + 1] = -thickness / 2;
+        (*vertices)[2*(resolution+i)*3 + 1] = thickness / 2;
         (*vertices)[2*(resolution+i)*3 + 2] = r1 * sin(angle);
 
         (*vertices)[(2*(resolution+i)+1)*3 + 0] = r2 * cos(angle);
-        (*vertices)[(2*(resolution+i)+1)*3 + 1] = -thickness / 2;
+        (*vertices)[(2*(resolution+i)+1)*3 + 1] = thickness / 2;
         (*vertices)[(2*(resolution+i)+1)*3 + 2] = r2 * sin(angle);
     }
 
@@ -73,16 +74,16 @@ void buildVertices(float r1, float r2, float thickness, int resolution, float **
         (*indeces)[0][2*i + 1] = 2*i_mod + 1;
 
         //Bottom side
-        (*indeces)[1][2*i] = 2 * (resolution + i_mod);
-        (*indeces)[1][2*i + 1] = 2 * (resolution + i_mod) + 1;
+        (*indeces)[1][2*i] = 2*resolution + 2*i_mod;
+        (*indeces)[1][2*i + 1] = 2*resolution + 2*i_mod + 1;
 
         //Inner Wall
         (*indeces)[2][2*i] = 2 * i_mod;
-        (*indeces)[2][2*i + 1] = 2 * (resolution + i_mod);
+        (*indeces)[2][2*i + 1] = 2*resolution +  2*i_mod;
 
         //Outer Wall
-        (*indeces)[2][2*i] = 2 * i_mod + 1;
-        (*indeces)[2][2*i + 1] = 2*(resolution + i_mod) + 1;
+        (*indeces)[3][2*i] = 2 * i_mod + 1;
+        (*indeces)[3][2*i + 1] = 2*resolution + 2*i_mod + 1;
     }
 
 }
