fd5ac9d632eef4dc7f55276f9044401166360746
abderrahman504 <rahmankhsherb@gmail.com>
2024-06-14 23:24:52 +0300

    Add entrypoint function for windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,23 @@
 #include "Game/MainGameScene.h"
 #include "Game/LightTestScene.cpp"
 #include "Game/UITestScene.cpp"
+#include "Game/SoundManager.h"
 #include <stdio.h>
 #include <GL/freeglut.h>
-#include "Game/SoundManager.h"
+
+#include <windows.h>
 
 
 using namespace Engine;
 using namespace Engine::DataTypes;
 using namespace Game;
 
-int main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-     if (!SoundManager::initialize()) {
+    if (!SoundManager::initialize()) {
         return -1;
     }
+
     initEngine(argc, argv);
     SceneHead* scene = new Game::MainGameScene();
     // SceneHead* scene = new Game::MainGameScene();
@@ -25,6 +28,16 @@ int main(int argc, char** argv)
     setMainScene(scene);
     SoundManager::cleanup();
     return 0;
+}
 
-
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow)
+{
+    if (!SoundManager::initialize()) {
+        return -1;
+    }
+    int argc = 0;
+    char* argv[1] = {(char*) lpCmdLine};
+    
+    main(argc, argv);
+    return 0;
 }
